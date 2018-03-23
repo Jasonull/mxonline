42c7d7f0e119bbc6c56cda08bcc12b4ebd0ed801
jiayinxi <jiayinxi@testbird.com>
2018-03-23 10:22:51 +0800

    login with email

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+from .models import UserProfile
+
+
+class CustomBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
 
 
 # Create your views here.
-
-
 def login(request):
     if request.method == "POST":
         user_name = request.POST.get("username", "")
@@ -13,5 +25,7 @@ def login(request):
         if user is not None:
             auth_login(request, user)
             return render(request, "index.html")
+        else:
+            return render(request, "login.html", {"msg": "用户名或密码错误"})
     elif request.method == "GET":
         return render(request, "login.html", {})
